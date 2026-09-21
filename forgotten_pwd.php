e46aececd706d2523bee0cf3e8fd848f72f0adc2
Matteo Yanni <matteo.yanni@etu.univ-amu.fr>
2026-09-21 08:27:52 +0200

    fix(forgotten_pwd)

--- forgotten_pwd.php
+++ forgotten_pwd.php
@@ -1,32 +1,33 @@
 <?php
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
+
+// Config dotenv
 require 'vendor/autoload.php';
 $dotenv = DotenvVault\DotenvVault::createImmutable(__DIR__);
 $dotenv->safeLoad();
-require 'vendor/autoload.php';
+
 
 $mail = new PHPMailer(true);
-$mail->SMTPDebug = 2;
 
 try {
     // Configuration du serveur SMTP Alwaysdata
     $mail->isSMTP();
     $mail->Host       = "{$_SERVER['MAIL_HOST']}";
     $mail->SMTPAuth   = true;
-    $mail->Username   = "{$_SERVER['MAIL_ADDRESS']}"; // Votre email Alwaysdata
+    $mail->Username   = "{$_SERVER['MAIL_ADDRESS']}";
     $mail->Password   =
-        "{$_SERVER['MAIL_PASSWORD']}";             // Votre mot de passe email
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;      // Sécurisation SSL
-    $mail->Port       = "{$_SERVER['SSL_PORT']}";                              // Port pour SSL
+        "{$_SERVER['MAIL_PASSWORD']}";
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+    $mail->Port       = "{$_SERVER['SSL_PORT']}";
 
     // Destinataires
     $mail->setFrom("{$_SERVER['MAIL_ADDRESS']}", 'ApocalypseHorsemen');
     $mail->addAddress('matteoyni@gmail.com');
 
     // Contenu de l'e-mail
-    $mail->isHTML(true);                                  // Format HTML actif
-    $mail->CharSet = 'UTF-8';                             // Gestion des accents
+    $mail->isHTML(true);
+    $mail->CharSet = 'UTF-8';
     $mail->Subject = 'Test d\'envoi';
     $mail->Body    = '<h1>Bonjour</h1><p>Ceci est un e-mail envoyé via le SMTP d\'Alwaysdata !</p>';
     $mail->AltBody = 'Ceci est la version texte brut pour les clients e-mail non compatibles HTML';
